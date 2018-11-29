bd32bfa9c8b535a7e52d6d83bc1f99d0424c0b8e
huyumars <huyumars@gmail.com>
2018-11-29 05:09:41 +0000

    install shell

--- install.py
+++
@@ -1,4 +0,0 @@
-sudo apt install python
-sudo apt install cmake
-sudo apt-get install make
-./.vim/bundle/YouCompleteMe/install.sh --clang-completer
---
+++ install.sh
@@ -0,0 +1,23 @@
+#create link
+
+back_and_create_link(){
+  mv ~/$1 ~/$1.bk
+  ln -s $PWD/$1  ~/$1
+  echo "create links for" $1
+}
+
+back_and_create_link  .vim
+back_and_create_link  .vimrc
+back_and_create_link  .bash_profile
+back_and_create_link  .tmux.conf
+
+#update submodules
+git submodule update --init --recursive
+#ycm
+echo "install ycm"
+sudo apt install python
+sudo apt install cmake
+sudo apt-get install make
+sudo apt install python-dev
+sudo apt install clang
+./.vim/bundle/YouCompleteMe/install.sh --clang-completer
