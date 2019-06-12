086e593bc39285d3a0fa4eb85beb32e4a050e0ad
yhu2 <yhu2@ebay.com>
2019-06-12 22:24:49 +0800

    add -r to scp

--- scp.sh
+++ scp.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/expect
 set timeout 30
-spawn scp -P [lindex $argv 0] [lindex $argv 4] [lindex $argv 1]@[lindex $argv 2]:[lindex $argv 5]
+spawn scp -r -P [lindex $argv 0] [lindex $argv 4] [lindex $argv 1]@[lindex $argv 2]:[lindex $argv 5]
 expect {
         "(yes/no)?"
         {send "yes\n";exp_continue}
